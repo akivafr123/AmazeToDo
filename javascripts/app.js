702e8224ef482c033be555227a41413e88062017
Akiva Friedman-Rosner <arosner.akiva@gmail.com>
2016-03-28 02:58:07 -0400

    Demonstrate finding information about parent element

--- javascripts/app.js
+++ javascripts/app.js
@@ -4,9 +4,26 @@ var main = function() {
 	$(".tabs a span").toArray().forEach(function(element) {
 		// create a click event handler for this element
 		$(element).on("click", function() {
+			// since we're using the jQuery version of element
+			// we'll go ahead and create a temporary variable
+			// so we don't need to keep recreating it
+			var $element = $(element);
+
+
 			$(".tabs a span").removeClass("active");
-			$(element).addClass("active");
+			$element.addClass("active");
 			$("main .content").empty();
+			
+			if ($element.parent().is(":nth-child(1)")) {
+				console.log("FIRST TAB CLICKED!");
+			}
+			else if ($element.parent().is(":nth-child(2)")) {
+				console.log("SECOND TAB CLICKED");
+			}
+			else if ($element.parent().is(":nth-child(3)")) {
+				console.log("THIRD TAB CLICKED");
+			}
+
 			return false;
 		});
 	});
